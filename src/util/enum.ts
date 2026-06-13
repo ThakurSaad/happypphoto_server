2b9ac610c3fbcb043d69d69f45c21d1e62583efc
ThakurSaad <thakursaad613@gmail.com>
2026-06-13 13:03:30 +0600

    first commit

---
+++ src/util/enum.ts
@@ -0,0 +1,43 @@
+const EnumUserRole = {
+  USER: "USER",
+  PROPERTY_HOST: "PROPERTY_HOST",
+  DRIVER: "DRIVER",
+  MERCHANT: "MERCHANT",
+  ADMIN: "ADMIN",
+};
+
+const EnumPaymentStatus = {
+  SUCCEEDED: "succeeded",
+  UNPAID: "unpaid",
+};
+
+const EnumSocketEvent = {
+  CONNECTION: "connection",
+  DISCONNECT: "disconnect",
+
+  SOCKET_ERROR: "socket_error",
+  ONLINE_STATUS: "online_status",
+  UPDATE_LOCATION: "update_location",
+
+  START_CHAT: "start_chat",
+  SEND_MESSAGE: "send_message",
+};
+
+const EnumLoginProvider = {
+  LOCAL: "local",
+  GOOGLE: "google",
+  APPLE: "apple",
+};
+
+const EnumUserAccountStatus = {
+  VERIFIED: "verified",
+  UNVERIFIED: "unverified",
+};
+
+export {
+  EnumUserRole,
+  EnumPaymentStatus,
+  EnumSocketEvent,
+  EnumLoginProvider,
+  EnumUserAccountStatus,
+};
